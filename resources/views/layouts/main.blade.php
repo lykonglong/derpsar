e16c3cd2825d81c1f0fabdb6e7e3b44f01085939
lykonglong@qq.com <lykonglong@qq.com>
2017-08-18 18:18:55 +0700

    test3

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -217,26 +217,7 @@
 
     @yield('content')
 
-    <!-- Bottom Banner Start -->
-
-    <div class="bottom-banner-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 col-sm-4"> <a href="#" class="bottom-banner-img"><img src="images/ads-04.jpg" alt="bottom banner"> <span class="banner-overly"></span>
-                        <div class="bottom-img-info">
-                            <h3>Electronic</h3>
-                            <h6>Sed diam nonummy nibh euismod tincidunt</h6>
-                            <span class="shop-now-btn">Shop Now</span> </div>
-                    </a> </div>
-                <div class="col-md-8 col-sm-8"> <a href="#" class="bottom-banner-img last"><img src="images/ads-05.jpg" alt="bottom banner"> <span class="banner-overly last"></span>
-                        <div class="bottom-img-info last">
-                            <h3>New Collection</h3>
-                            <h6>Lorem ipsum dolor sit amet, consectetuer adipiscing elit</h6>
-                            <span class="shop-now-btn">Shop Now</span> </div>
-                    </a> </div>
-            </div>
-        </div>
-    </div>
+    
 
     <!-- category area start -->
     <div class="jtv-category-area">
